d67c579cdc46a0f487b4f9df6553e16d46565559
Jim Holmström <jim.holmstroem@gmail.com>
2012-05-18 09:46:25 +0200

    tried to make it work in school but damn lag

--- diff_test.m
+++ diff_test.m
@@ -46,39 +46,41 @@ while(vid.FramesAcquired<=10000) % Stop after 100 frames
 %        seg_im = segments*round((data)/segments);
 %        diff_im= old_data -data;
        
-	grey = sum(data,3)/3;
- 
- 
-%        fourier = fft2(sum(data,3));
-%        fourier2= fft2(sum(seg_im,3));
+        grey = sum(data,3)/3;
+        grey = grey/255; 
+        size(gray); 
+    %        fourier = fft2(sum(data,3));
+    %        fourier2= fft2(sum(seg_im,3));
+            
+        %subplot(1,2,1);
         
-	%subplot(1,2,1);
-	showgrey(sqrt(imfilter(grey,fspecial('prewitt')).^2+imfilter(grey,fspecial('prewitt')).^2));
+        %imshow(grey);
+        showgrey(sqrt(imfilter(grey,fspecial('prewitt')).^2+imfilter(grey,fspecial('prewitt')).^2));
 
-	%data_r = data(:,:,1);
-	%data_g = data(:,:,2);
-	%data_b = data(:,:,3);
-	
-	%print 'tries to plot'
-	%subplot(1,2,2);scatter3(reshape(data_r,prod(size(data_r)),1),reshape(data_g,prod(size(data_g)),1),reshape(data_b,prod(size(data_b)),1),1);
-	%print 'done'	
+        %data_r = data(:,:,1);
+        %data_g = data(:,:,2);
+        %data_b = data(:,:,3);
+        
+        %print 'tries to plot'
+        %subplot(1,2,2);scatter3(reshape(data_r,prod(size(data_r)),1),reshape(data_g,prod(size(data_g)),1),reshape(data_b,prod(size(data_b)),1),1);
+        %print 'done'	
 
-	%sobel_filter = [-1 0 1;-2 0 2;-1 0 1];
-        %subplot(2,2,2);imshow(conv2(grey,sobel_filter+sobel_filter','same'));
-       
-	%subplot(2,2,3);
-	%scatter3(data(:,1,1),data(:,1,2),data(:,1,3));
+        %sobel_filter = [-1 0 1;-2 0 2;-1 0 1];
+            %subplot(2,2,2);imshow(conv2(grey,sobel_filter+sobel_filter','same'));
+           
+        %subplot(2,2,3);
+        %scatter3(data(:,1,1),data(:,1,2),data(:,1,3));
 
-	%subplot(2,2,4);
-	%imshow(diff_im);
+        %subplot(2,2,4);
+        %imshow(diff_im);
         
         %subplot(2,2,3);showfs(real(fourier));
         %subplot(2,2,4);showfs(real(fourier2));
         
-        
+    drawnow; 
     catch err
-	err.message
-	error('got problems');
+	    err.message
+	    error('got problems');
     end
     old_data = data;
 end
